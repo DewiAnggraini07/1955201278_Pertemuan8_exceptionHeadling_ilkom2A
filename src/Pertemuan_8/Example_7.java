f21f60b663ef8a7799ae11f809d641b8be216e6f
DewiAnggraini07 <dewiangga0712@gmail.com>
2020-06-25 10:17:07 +0700

    tugas pertemuan 8 latihan exceptionHeadling

---
+++ src/Pertemuan_8/Example_7.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan_8;
+
+/**
+ *
+ * @author HP
+ */
+public class Example_7 {
+    public static void main(String[] args) {  
+          
+        try  
+        {  
+        int data1=50/0; //may throw exception   
+  
+        }  
+             // handling the exception  
+        catch(Exception e)  
+        {  
+            // generating the exception in catch block  
+        int data2=50/0; //may throw exception   
+  
+        }  
+    System.out.println("rest of the code");  
+    }     
+}
+
